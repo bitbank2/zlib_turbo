b5ab37e4e902f6b8099c84c4fb72d9804f03b733
Laurence Bank <laurencebank@Laurences-Air.home>
2024-07-08 13:20:37 +0100

    Added copyright for original authors

--- src/zlib_turbo.h
+++ src/zlib_turbo.h
@@ -1,9 +1,36 @@
 //
-//  zlib_turbo.h
-//  zlib_turbo
+// zlib_turbo
+// An optimized version of zlib inflate
+// Created by Larry Bank on 4/19/24.
+// Copyright (C) 2024 BitBank Software, Inc.
 //
-//  Created by Laurence Bank on 4/19/24.
-//
+// Original code written by Mark Adler and Jean-loup Gailly
+/*   Copyright (C) 1995-2024 Jean-loup Gailly and Mark Adler
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+
+  Jean-loup Gailly        Mark Adler
+  jloup@gzip.org          madler@alumni.caltech.edu
+
+
+  The data format used by the zlib library is described by RFCs (Request for
+  Comments) 1950 to 1952 in the files http://tools.ietf.org/html/rfc1950
+  (zlib format), rfc1951 (deflate format) and rfc1952 (gzip format).
+*/
 
 #ifndef zlib_turbo_h
 #define zlib_turbo_h
